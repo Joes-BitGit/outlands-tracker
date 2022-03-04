844d5b287a8c6a905dcca0718edb60f0a1ef7c9e
Joes-BitGit <josephnalmeida@gmail.com>
2022-03-03 19:39:44 -0800

    production ready axios instance

---
+++ client/src/apis/PlayerFinder.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+// NODE_ENV = 'development'
+// NODE_ENV = 'production'
+
+// if in prod baseURL = /api/v1/profile
+// else baseURL = 'http://localhost:5555/api/v1/profile'
+
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "/api/v1/profile"
+    : "http://localhost:5555/api/v1/profile";
+
+export default axios.create({
+  baseURL,
+});
