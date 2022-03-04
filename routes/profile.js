41c6494c30133b72ce738afb26ce92add4c2fab4
Joes-BitGit <josephnalmeida@gmail.com>
2022-03-04 14:03:30 -0800

    console logs

--- routes/profile.js
+++ routes/profile.js
@@ -6,6 +6,7 @@ const fetch = require("node-fetch");
 router.get("/:platform/:gamertag", async (req, res) => {
   // console.log(req.params.platform, req.params.gamertag);
   // res.send("Hello");
+  console.log("route gets called first");
   try {
     const headers = {
       "TRN-Api-Key": process.env.TRACKER_API_KEY,
