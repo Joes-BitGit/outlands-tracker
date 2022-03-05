9bde051f83fd92f505795f079c00b4ae2be0bd82
Joes-BitGit <josephnalmeida@gmail.com>
2022-03-04 19:15:14 -0800

    resolve path to the new build location

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "public")));
   // handle SPA
   app.get(/.*/, function (req, res) {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.resolve(__dirname, "public", "index.html"));
   });
 }
 
